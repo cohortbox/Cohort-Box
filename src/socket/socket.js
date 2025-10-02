0bb06734b0583b51bcb7167c1dadc83bfe75b0e6
Hani Nadeem <hnadem180@gmail.com>
2025-10-03 02:21:44 +0500

    ALL-API-URL-CHANGES

--- src/socket/socket.js
+++ src/socket/socket.js
@@ -1,7 +1,7 @@
 import { io } from "socket.io-client";
 
 export const connectSocket = (accessToken) => {
-  return io(`${process.env.REACT_APP_API_BASE_URL}`, {
+  return io(`/`, {
     auth: { token: accessToken }, // send token with connection
     withCredentials: true,
   });
