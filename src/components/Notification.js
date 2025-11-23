8aa227e2b214c38b41f6c8df2c6e341ea86da26a
Hani Nadeem <hnadem180@gmail.com>
2025-11-23 16:58:50 +0500

    Notification-File

---
+++ src/components/Notification.js
@@ -0,0 +1,10 @@
+import './Notification.css';
+
+export default function Notification(){
+    return (
+        <div className='notification-container'>
+            <img className='notification-img'/>
+            <p className='notification-msg'></p>
+        </div>
+    )
+}
